b0ec0ba7ad6cf79a5e207fd659fa40173cc7cedf
KheyronSaligaBSIT1B <kheysaliga0@gmail.com>
2025-04-05 11:39:02 +0800

    Added methods for the traffic controller

--- src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
+++ src/main/java/aclcbukidnon/com/javafxactivity/controllers/CounterController.java
@@ -1,4 +1,38 @@
 package aclcbukidnon.com.javafxactivity.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class CounterController {
+
+    public Label Counter;
+    @FXML
+    private Label lblCounter;
+
+    @FXML
+    private Button btnIncrement, btnDecrement;
+
+    private int counter = 0;
+
+    @FXML
+    public void initialize() {
+        updateCounterLabel();
+    }
+
+    @FXML
+    private void increment() {
+        counter++;
+        updateCounterLabel();
+    }
+
+    @FXML
+    private void decrement() {
+        counter--;
+        updateCounterLabel();
+    }
+
+    private void updateCounterLabel() {
+        lblCounter.setText(String.valueOf(counter));
+    }
 }
